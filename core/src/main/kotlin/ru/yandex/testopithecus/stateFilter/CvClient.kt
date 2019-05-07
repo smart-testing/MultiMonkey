2b027db35d7f04fb2c7b14850e6d27e44073f127
Pavel Solikov <psolikov15@gmail.com>
2019-05-07 12:28:16 +0300

    Started implementing CvClient

---
+++ core/src/main/kotlin/ru/yandex/testopithecus/stateFilter/CvClient.kt
@@ -0,0 +1,23 @@
+package ru.yandex.testopithecus.stateFilter
+
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.RequestBody
+import okhttp3.Request
+
+
+class CvClient(private val cvServerAddress: String) {
+
+    val JSON = MediaType.get("application/json; charset=utf-8")
+
+    var client = OkHttpClient()
+
+    fun post(json: String): String {
+        val body = RequestBody.create(JSON, json)
+        val request = Request.Builder()
+                .url(cvServerAddress)
+                .post(body)
+                .build()
+        client.newCall(request).execute().use { response -> return response.body()!!.string() }
+    }
+}
