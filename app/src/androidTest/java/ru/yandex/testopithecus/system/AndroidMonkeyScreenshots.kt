ecfaea1775f494e0db4776c9c89152a9946c6670
Pavel Solikov <psolikov15@gmail.com>
2019-05-13 14:29:00 +0300

    Started implementing button life inspector

--- app/src/androidTest/java/ru/yandex/testopithecus/system/AndroidMonkeyScreenshots.kt
+++ app/src/androidTest/java/ru/yandex/testopithecus/system/AndroidMonkeyScreenshots.kt
@@ -4,6 +4,7 @@ import androidx.test.uiautomator.By
 import androidx.test.uiautomator.StaleObjectException
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.Until
+import ru.yandex.testopithecus.businesslogictesting.ButtonLifeInspector
 import ru.yandex.testopithecus.monkeys.state.StateModelMonkey
 import ru.yandex.testopithecus.stateenricher.SimpleEnricher
 import ru.yandex.testopithecus.ui.Monkey
@@ -11,10 +12,14 @@ import ru.yandex.testopithecus.ui.UiState
 import java.io.File
 
 class AndroidMonkeyScreenshots(private val device: UiDevice, private val applicationPackage: String,
-                               private val apk: String, private val screenshotDir: File, url: String) :
+                               private val apk: String,
+                               private val screenshotDir: File,
+                               url: String,
+                               useButtonLifeInspector: Boolean = false) :
         AndroidMonkey(device, applicationPackage, apk) {
 
     private val model: Monkey = StateModelMonkey(SimpleEnricher(url))
+    private val buttonLifeInspector: ButtonLifeInspector = ButtonLifeInspector(useButtonLifeInspector)
 
     companion object {
         const val ANDROID_LOCALHOST = "10.0.2.2"
@@ -37,7 +42,10 @@ class AndroidMonkeyScreenshots(private val device: UiDevice, private val applica
         val action = generateAction(uiState)
         val id = action.id?.toInt()
         val element = id?.let { elements[id] }
+        buttonLifeInspector.loadScreenshotBeforeAction()
         AndroidActionPerformer(device, applicationPackage, apk, element).perform(action)
+        buttonLifeInspector.loadScreenshotAfterAction()
+        buttonLifeInspector.assertButtonLives()
     }
 
     override fun generateAction(uiState: UiState) = model.generateAction(uiState)
---
+++ core/src/main/kotlin/ru/yandex/testopithecus/businesslogictesting/ButtonLifeInspector.kt
@@ -0,0 +1,13 @@
+package ru.yandex.testopithecus.businesslogictesting
+
+class ButtonLifeInspector(private val disabled: Boolean = true) {
+
+    fun loadScreenshotAfterAction() {
+    }
+
+    fun loadScreenshotBeforeAction() {
+    }
+
+    fun assertButtonLives() {
+    }
+}
