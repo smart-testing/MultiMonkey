a7b19da5b2609cc8f1f4b7267fd4da7c482cb0c0
Pavel Solikov <psolikov15@gmail.com>
2019-05-13 16:56:25 +0300

    Added implementation of ButtonLifeInspector

--- app/src/androidTest/java/ru/yandex/testopithecus/system/AndroidMonkeyScreenshots.kt
+++ app/src/androidTest/java/ru/yandex/testopithecus/system/AndroidMonkeyScreenshots.kt
@@ -6,6 +6,7 @@ import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.Until
 import ru.yandex.testopithecus.businesslogictesting.ButtonLifeInspector
 import ru.yandex.testopithecus.monkeys.state.StateModelMonkey
+import ru.yandex.testopithecus.stateenricher.CvClient
 import ru.yandex.testopithecus.stateenricher.SimpleEnricher
 import ru.yandex.testopithecus.ui.Monkey
 import ru.yandex.testopithecus.ui.UiState
@@ -19,7 +20,10 @@ class AndroidMonkeyScreenshots(private val device: UiDevice, private val applica
         AndroidMonkey(device, applicationPackage, apk) {
 
     private val model: Monkey = StateModelMonkey(SimpleEnricher(url))
-    private val buttonLifeInspector: ButtonLifeInspector = ButtonLifeInspector(useButtonLifeInspector)
+    private val urlButtonLifeInspector = "http://${AndroidMonkeyScreenshots.ANDROID_LOCALHOST}:5000/button-alive"
+    private val buttonLifeInspector: ButtonLifeInspector = ButtonLifeInspector(useButtonLifeInspector,
+            ::takeScreenshot,
+            CvClient(urlButtonLifeInspector))
 
     companion object {
         const val ANDROID_LOCALHOST = "10.0.2.2"
@@ -34,6 +38,10 @@ class AndroidMonkeyScreenshots(private val device: UiDevice, private val applica
         }
     }
 
+    private fun takeScreenshot(): String {
+        return AndroidElementParser.takeScreenshot(screenshotDir, device)
+    }
+
     private fun performActionImpl() {
         val elements = device.findObjects(By.pkg(applicationPackage))
         device.wait(Until.hasObject(By.pkg(applicationPackage).depth(0)), LONG_WAIT)
@@ -42,6 +50,7 @@ class AndroidMonkeyScreenshots(private val device: UiDevice, private val applica
         val action = generateAction(uiState)
         val id = action.id?.toInt()
         val element = id?.let { elements[id] }
+        //ToDo обработать SKIP
         buttonLifeInspector.loadScreenshotBeforeAction()
         AndroidActionPerformer(device, applicationPackage, apk, element).perform(action)
         buttonLifeInspector.loadScreenshotAfterAction()
--- core/src/main/kotlin/ru/yandex/testopithecus/businesslogictesting/ButtonLifeInspector.kt
+++ core/src/main/kotlin/ru/yandex/testopithecus/businesslogictesting/ButtonLifeInspector.kt
@@ -1,13 +1,44 @@
 package ru.yandex.testopithecus.businesslogictesting
 
-class ButtonLifeInspector(private val disabled: Boolean = true) {
+import org.json.JSONObject
+import ru.yandex.testopithecus.stateenricher.CvClient
+import ru.yandex.testopithecus.utils.deserializeJSONObject
+import java.lang.RuntimeException
 
-    fun loadScreenshotAfterAction() {
-    }
+class ButtonLifeInspector(private val disabled: Boolean = true,
+                          private val screenshotPerformer: () -> String,
+                          private val cvClient: CvClient = CvClient("")) {
+
+    private var before: String = ""
+    private var after: String = ""
 
     fun loadScreenshotBeforeAction() {
+        if (disabled) return
+        val screenshot = screenshotPerformer()
+        before = screenshot
+    }
+
+    fun loadScreenshotAfterAction() {
+        if (disabled) return
+        val screenshot = screenshotPerformer()
+        after = screenshot
+    }
+
+    private fun createRequest(): String {
+        return "{ \"before\": \"$before\"," +
+                "\"after\": \"$after\"" +
+                "}"
     }
 
     fun assertButtonLives() {
+        if (disabled) return
+        if (before == "" || after == "") throw RuntimeException("ButtonLifeInspector: either \"before\" or \"after\" is empty")
+        val request = createRequest()
+        after = ""
+        before = ""
+        val response = cvClient.post(request)
+        print(response)
+        val isAlive = deserializeJSONObject(JSONObject(response))["button-alive"] as String
+        assert(isAlive.toInt() == 1)
     }
 }
